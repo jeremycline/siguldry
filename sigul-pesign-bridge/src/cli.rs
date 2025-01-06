c93abbbe6730c1c1edb0a88e11a40c72691afa7a
Jeremy Cline <jeremycline@linux.microsoft.com>
2025-01-06 16:42:50 -0500

    Expand CLI help text for the "listen" command
    Signed-off-by: Jeremy Cline <jeremycline@linux.microsoft.com>

--- sigul-pesign-bridge/src/cli.rs
+++ sigul-pesign-bridge/src/cli.rs
@@ -37,29 +37,31 @@ pub(crate) struct Cli {
 #[derive(clap::Subcommand, Debug)]
 pub(crate) enum Command {
     /// Run the service.
+    ///
+    /// The service provides a Unix socket at, by default, `/run/pesign/socket`.
+    /// `pesign-client` can be used to connect to this socket and request
+    /// signatures for PE applications. Unlike `pesign`, this implementation
+    /// forwards the request to a Sigul signing server.
     Listen {
         /// The service's runtime directory.
         ///
-        /// This is where the service socket is created, along with any
-        /// temporary files. The socket name will be `socket` under this
-        /// directory.
+        /// This is where the service socket is created, along with any temporary
+        /// files. The socket name will be `socket` under this directory.
         ///
         /// Anyone with access to the socket can get a PE application signed, so
         /// care should be taken to ensure this directory is not world-readable.
-        /// This directory should be read/writeable only to the service
-        /// owner/group. Temporary files created within this directory are
-        /// readable only to the owner, so users in the group can only acceess
-        /// the socket.
+        /// This directory should be read/writeable only to the service owner/group.
+        /// Temporary files created within this directory are readable only to the
+        /// owner, so users in the group can only acceess the socket.
         ///
-        /// When run under systemd, providing a `RuntimeDirectory=` directive
-        /// will set the environment variable automatically for you.
+        /// When run under systemd, providing a `RuntimeDirectory=` directive will
+        /// set the environment variable automatically for you.
         #[arg(long, short, env = "RUNTIME_DIRECTORY")]
         runtime_directory: PathBuf,
     },
     /// Print the current service configuration to standard output.
     ///
-    /// If no config file is provided, the defaults are printed. For
-    /// complete details on each configuration option, refer to the the
-    /// documentation.
+    /// If no config file is provided, the defaults are printed. For complete
+    /// details on each configuration option, refer to the the documentation.
     Config,
 }
